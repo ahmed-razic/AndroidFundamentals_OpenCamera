70e93f34ff8cb753d835a9dd27725dc279d6cfe3
ahmed-razic <Ibrahim2012*>
2021-02-07 21:49:24 +0100

    added button to open camera

--- app/src/main/java/com/example/android/opencamera/MainActivity.java
+++ app/src/main/java/com/example/android/opencamera/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.android.opencamera;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mTakePhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTakePhoto = findViewById(R.id.button);
+    }
+
+    public void takePhoto(View view) {
+
+        Intent intent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
+        startActivity(intent);
+
     }
 }
